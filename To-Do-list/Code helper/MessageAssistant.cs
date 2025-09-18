b4cc2e121a627ad510318fc1fee1ef1ecfbf7d3f
Aleksey Shapkin <ceylonneck@gmail.com>
2025-09-18 19:30:32 +0500

    feat(MessageAssistant): added color assistant

---
+++ To-Do-list/Code helper/MessageAssistant.cs
@@ -0,0 +1,38 @@
+﻿namespace To_Do_list.Code_helper
+{
+    public static class MessageAssistant
+    {
+        /// <summary>
+        /// Выводит сообщение зелёного цвета, нужен для обозначения успешности действия.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void GreenMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Выводит сообщение красного цвета, нужен для обозначения неудачности действия.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void RedMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Выводит сообщение синего цвета, нужен вывода различной информации.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void BlueMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+    }
+}
