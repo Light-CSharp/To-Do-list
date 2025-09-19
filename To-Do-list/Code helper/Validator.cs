601cd89aa9747f07d82415ae7732b8599b8b1c40
Aleksey Shapkin <ceylonneck@gmail.com>
2025-09-19 19:13:22 +0500

    feat(Validator): added validator values

---
+++ To-Do-list/Code helper/Validator.cs
@@ -0,0 +1,58 @@
+﻿namespace To_Do_list.Code_helper
+{
+    public static class Validator
+    {
+        public static string GetString()
+        {
+            bool isEmpty;
+            string line;
+
+            do
+            {
+                line = Console.ReadLine()!.Trim();
+                isEmpty = string.IsNullOrEmpty(line);
+                if (isEmpty)
+                {
+                    MessageAssistant.RedMessage("Неверный формат строки! Попробуйте ещё раз: ");
+                }
+            } while (isEmpty);
+
+            return line;
+        }
+
+        public static int GetInt()
+        {
+            bool isCorrect;
+            int number;
+
+            do
+            {
+                isCorrect = int.TryParse(Console.ReadLine(), out number);
+                if (isCorrect)
+                {
+                    MessageAssistant.RedMessage("Неверный формат числа! Попробуйте ещё раз: ");
+                }
+            } while (isCorrect);
+            
+            return number;
+        }
+
+        public static int GetIntInRange(int minimumNumber, int maximumNumber)
+        {
+            bool isCorrect;
+            int number;
+
+            do
+            {
+                number = GetInt();
+                isCorrect = number >= minimumNumber && number <= maximumNumber;
+                if (!isCorrect)
+                {
+                    MessageAssistant.RedMessage($"Число должно входить в диапазон: {minimumNumber}-{maximumNumber}! Попробуйте ещё раз: ");
+                }
+            } while (!isCorrect);
+
+            return number;
+        }
+    }
+}
