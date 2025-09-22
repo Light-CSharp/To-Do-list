8c8099304836dffd08ced88983cbd75786f87e4d
Aleksey Shapkin <ceylonneck@gmail.com>
2025-09-22 20:31:26 +0500

    refactor: move interface to folder

---
+++ To-Do-list/Basic logic/Interfaces/ITaskManager.cs
@@ -0,0 +1,10 @@
+﻿namespace To_Do_list.Basic_logic.Interfaces
+{
+    public interface ITaskManager
+    {
+        void AddTask(Task task);
+        void ReadTasks();
+        void UpdateTask(int taskIndex, Task task);
+        void DeleteTask(int taskIndex);
+    }
+}
---
+++ To-Do-list/Basic logic/Interfaces/ITaskStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace To_Do_list.Basic_logic.Interfaces
+{
+    public interface ITaskStorage
+    {
+        Task? AddTask(Task task);
+        List<Task>? AddTasks(List<Task> tasks);
+        void ReadTasks();
+    }
+}
